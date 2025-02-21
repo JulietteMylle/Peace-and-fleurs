a5b0366d4874eeb9c28fedcf922e9f1d81d7317d
Lea-git <lea@druffin.fr>
2025-02-21 14:13:21 +0100

    Accueil bis

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,14 +2,14 @@
 
 import "./App.css";
 
-
 import RoutesPosts from "./Router/Routes.jsx";
-import RoutesPosts from "./Router/RoutesPosts.jsx";
 import 'font-awesome/css/font-awesome.min.css';
+
 
 
 function App() {
   return (
+
     <>
       <RoutesPosts />
     </>
--- front-end/src/pages/accueil.jsx
+++ front-end/src/pages/accueil.jsx
@@ -1,27 +1,27 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
 import fleurApi from "../services/fleursApi";
+import Header from "./Header";
+import Footer from "./Footer";
 
 
 const Accueil = () => {
-    const navigate = useNavigate();
-  
-    // Fonction de navigation vers la page de détails
-    const handleNavigate = () => {
-      navigate("/Detail");
-    };
-const homePage = () => {
-    const [flowers, setFlowers] = useState();
-    const {id} = useParams();
-    const navigate = useNavigate();
-    useEffect(() => {
-        PostsApi.getPostsById(id).then((data) => setFlowers(data));
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [flowers, setFlowers] = useState([]);
+  useEffect(() => {
+  fleurApi.getFlowers(id).then((data) => setFlowers(data)).catch((error) =>console.error( "Erreur lors de la récupération des fleurs", error));
 
-    }, [id]);
-}
+}, [id]);
+
+const handleNavigate =(id) => {
+navigate('/Detail/${id}')
+};
+
 
 return (
     <div className="homepage">
+       <Header/>
       <h1>Bienvenue dans notre boutique de fleurs</h1>
       <div className="flower-list">
         {flowers.length === 0 ? (
@@ -33,11 +33,12 @@ return (
               <h3>{flower.name}</h3>
               <p>{flower.description}</p>
               <p>{flower.price}€</p>
-              <button>Voir les détails</button>
+              <button onClick={() => handleNavigate(flower._id)}>Voir les détails</button>
             </div>
           ))
         )}
       </div>
+      <Footer/>
     </div>
   );
     };
